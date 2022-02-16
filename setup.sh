54594e11bc414a60a4c7a7e20931b5599e8750ca
Zhao Wei Liew <zhaoweiliew@gmail.com>
2022-02-16 09:02:29 +0800

    himalaya: set up himalaya email client

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 
 ln -s $(pwd)/tmux/.tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.config/nvim
 ln -s $(pwd)/neovim/init.vim ~/.config/nvim/init.vim
+
 ln -s $(pwd)/git/.gitconfig ~/.gitconfig
+
 ln -s $(pwd)/vim/.vimrc ~/.vimrc
+
+mkdir -p ~/.config/himalaya
+ln -s $(pwd)/himalaya/config.toml ~/.config/himalaya/config.toml
