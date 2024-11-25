3e3aa9f5f7f1514a48e852b86e5ad7500d67e75c
Zhao Wei Liew <zhaoweiliew@gmail.com>
2024-11-25 11:33:18 +0800

    fish: activate emsdk

--- fish/config.fish
+++ fish/config.fish
@@ -35,6 +35,8 @@ set -x EDITOR nvim
 fish_add_path $HOME/.bun/bin
 set -x BUN_INSTALL "$HOME/.bun"
 
+source $HOME/_me/gh/emsdk/emsdk_env.fish
+
 if test -f $HOME/_me/private_config.fish
     source $HOME/_me/private_config.fish
 end
